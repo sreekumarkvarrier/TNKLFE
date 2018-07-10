75f1c8cb3978f3f4682040fcd0f23ce25b15c195
Ambarish V <ambarish.v@thinkpalm.com>
2018-07-10 10:19:02 +0530

    Changes by Ambarish

---
+++ TNKLFEAPI/TkLfeDAL/UserDAL.cs
@@ -0,0 +1,21 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TkLfeDAL
+{
+    public class UserDAL
+    {
+        public static DataSet GetUsers()
+        {
+            DataSet ds = new DataSet();
+            DBProvider dbProvider = new DBProvider();
+            ds = dbProvider.ExecuteDataset("TkLfUserList");
+            return ds;
+        }
+    }
+}
